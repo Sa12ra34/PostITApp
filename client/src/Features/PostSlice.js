1323b1fc1e67bc2b732070c9c772819c45457ea6
Sa12ra34 <gamailhalhsaeg@gmail.com>
2025-05-12 13:06:16 +0400

    Update PostSlice.js

--- client/src/Features/PostSlice.js
+++ client/src/Features/PostSlice.js
@@ -69,7 +69,7 @@ const postSlice = createSlice({
 
 export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
     try {
-        const response = await axios.post('http://localhost:3001/savePost', {
+        const response = await axios.post(`https://postitapp-server-ql42.onrender.com/savePost`, {
             postMsg: postData.postMsg,
             email: postData.email,
         });
@@ -83,7 +83,7 @@ export const savePost = createAsyncThunk("posts/savePost", async (postData) => {
 
 export const getPosts = createAsyncThunk("post/getPosts", async () => {
     try {
-        const response = await axios.get("http://localhost:3001/getPosts");
+        const response = await axios.get(`https://postitapp-server-ql42.onrender.com/getPosts`);
         return response.data.posts;
         console.log(response);
     } catch (error) {
@@ -96,7 +96,7 @@ export const likePost = createAsyncThunk("posts/likePost", async (postData) => {
 try {
     //Pass along the URL the postId
     const response = await axios.put(
-        `http://localhost:3001/likePost/${postData.postId}`,
+        `https://postitapp-server-ql42.onrender.com/likePost/${postData.postId}`,
         {
         userId: postData.userId,
         }
