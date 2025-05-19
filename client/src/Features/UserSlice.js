c6830cd288184b47e82854be7ec0f88711436a01
Sa12ra34 <gamailhalhsaeg@gmail.com>
2025-05-19 11:38:39 +0400

    Update UserSlice.js

--- client/src/Features/UserSlice.js
+++ client/src/Features/UserSlice.js
@@ -1,161 +1,93 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { UsersData } from "../Exampledata";
-import { createAsyncThunk} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-//const initialState = { value: UsersData }; //list of user is an object with empty array as initial value
-const initialState = {
-  user: {},
-  isLoading: false,
-  isSuccess: false,
-  isError: false,
+const initialState = { 
+  services: [],
+    posts: [],
+    likes: [],
 };
 
-export const registerUser=createAsyncThunk("users/registerUsers",
-  async (userData)=>{
-    try{
-      const response= await
-      axios.post(`https://postitapp-server-ql42.onrender.com/registerUser`,{
-        name:userData.name,
-        email:userData.email,
-        password:userData.password,
-      });
-      console.log(response);
-      const user=response.data.user;
-      return user;
-    }catch(error){
-      console.log(error)
-    }
-  }
-);
-export const login=createAsyncThunk("users/login",
-  async (userData)=>{
-    try{
-      const response =await
-      axios.post(`https://postitapp-server-ql42.onrender.com/login`,{
-        email:userData.email,
-        password:userData.password,
-      });
-      const user =response.data.user;
-      console.log(response);
-      return user;
-    }catch(error){
-      const errorMessage ="Invaild credentials";
-      alert(errorMessage);
-      throw new Error(errorMessage);
-    }
-  }
-);
-export const logout =createAsyncThunk("/users/logout",async()=>{
-  try{
-    const response=await
-    axios.post(`https://postitapp-server-ql42.onrender.com/logout`);
-  }catch(error){}
+
+const ServicesSlice = createSlice({
+    name: "services",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(saveService.pending, (state) => {
+            state.status = "loading";
+          })
+          .addCase(saveService.fulfilled, (state, action) => {
+            console.log(action.payload);
+            state.status = "succeeded";
+            // Update the state with fetched posts adding the latest post in the beginning
+            state.posts.unshift(action.payload);
+          })
+          .addCase(saveService.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+          })
+    
+          .addCase(getServices.pending, (state) => {
+            state.status = "loading";
+          })
+          .addCase(getServices.fulfilled, (state, action) => {
+            state.status = "succeeded";
+            // Update the state with fetched posts
+            console.log(action.payload);
+            state.posts = action.payload;
+          })
+          .addCase(getServices.rejected, (state, action) => {
+            state.status = "failed";
+            state.error = action.error.message;
+          });
+          
+    
+    },
 });
 
-export const updateUserProfile = createAsyncThunk(
-  "user/updateUserProfile", // Action type string for Redux
-  async (userData) => {
+// Thunks for Services
+export const saveService = createAsyncThunk("services/saveService", async (serviceData) => {
     try {
-      const response = await axios.put(
-        `https://postitapp-server-ql42.onrender.com/updateUserProfile/${userData.email}`, 
-        {
-      
-          email: userData.email,
-          name: userData.name,
-          password: userData.password,
-          profilePic: userData.profilePic,
-        },
-        {
-          headers: { 
-            "Content-Type": "multipart/form-data", 
-          },
-        }
-      );
+        const response = await axios.post('http://localhost:3001/saveService', {
+            serviceName:serviceData.serviceName ,
+            date:serviceData.date ,
+            seatsAvailable:serviceData.seatsAvailable ,
+            email:serviceData.email ,
 
-      const user = response.data.user;
-
-    
-      return user;
+        
+        });
+        return response.data.services; // Return the service to Redux
     } catch (error) {
-    
-      console.log(error);
+        console.log(error);
     }
-  }
-);
-
-export const userSlice = createSlice({
-  name: "users", //name of the state
-  initialState, // initial value of the state
-  reducers: {
-    addUser:(state,action)=>{
-      state.value.push(action.payload);
-    },
-    deleteUser:(state,action)=>{
-      state.value=state.value.filter((user)=>user.email !== action.payload);
-    },
-    updateUser:(state,action)=>{
-      state.value.map((user)=>{
-        if(user.email === action.payload.email){
-          user.name=action.payload.name;
-          user.password=action.payload.password;
-        }
-      });
-    },
-  },
-  extraReducers:(builder)=>{
-    builder
-    .addCase(registerUser.pending, (state)=>{
-      state.isLoading=true;
-    })
-    .addCase(registerUser.fulfilled,(state,action)=>{
-      state.isLoading=true;
-    })
-    .addCase(registerUser.rejected,(state)=>{
-      state.isLoading=false;
-    })
-
-    .addCase(login.pending, (state)=>{
-      state.isLoading=true;
-    })
-    .addCase(login.fulfilled,(state,action)=>{
-      state.user=action.payload;
-      state.isLoading=false;
-      state.isSuccess=true;
-    })
-    .addCase(login.rejected,(state)=>{
-      state.isLoading=false;
-      state.isError=true;
-    })
-
-  .addCase(logout.pending,(state)=>{
-    state.isLoading=true;
-  })
-  .addCase(logout.fulfilled,(state)=>{
-    state.user ={};
-    state.isLoading =false;
-    state.isSuccess =false;
-  })
-  .addCase(logout.rejected,(state)=>{
-    state.isLoading=false;
-    state.isError=true;
-  })
-
-  .addCase(updateUserProfile.pending, (state) => {
-    state.isLoading = true;
-  })
-  .addCase(updateUserProfile.fulfilled, (state, action) => {
-    state.user = action.payload;
-    state.isLoading = false;
-  })
-  .addCase(updateUserProfile.rejected, (state) => {
-    state.isLoading = false;
-    state.isError = true;
-  });
-
-}
-
 });
 
-export const{addUser,deleteUser,updateUser}=userSlice.actions;
-export default userSlice.reducer;
+export const getServices = createAsyncThunk("services/getServices", async () => {
+    try {
+        const response = await axios.get("http://localhost:3001/getServices");
+        return response.data.services;
+    } catch (error) {
+        console.log(error);
+    }
+});
+export const likePost = createAsyncThunk("posts/likePost", async (postData) => {
+try {
+    //Pass along the URL the postId
+    const response = await axios.put(
+        `http://localhost:3001/likePost/${postData.postId}`,
+        {
+        userId: postData.userId,
+        }
+    );
+    const post = response.data.post;
+    return post;
+} catch (error) {
+    console.log(error);
+}
+});
+
+
+
+
+export default ServicesSlice.reducer;
